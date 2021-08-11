70b589368f7f31565e7581335d9b3471faf7c926
Anton <golubin1990@gmail.com>
2021-08-11 14:09:27 -0400

    Implemented HTML/CSS mark up for displaying tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -20,8 +20,25 @@ const tweetData = {
 // A function to dynamically create HTML/CSS markup to display tweets
 const createTweetElement = function(input) {
 
-
-
+  const tweetBody = `
+                    <article class = "article-tweet">
+                        <header>
+                          <div>${input.user.avatars}</div>
+                          <div id = 'handle'>${input.user.handle}</div>
+                        </header>
+                          <div>${input.content.text}</div>
+                            <hr class = "divider2">
+                        <footer>
+                          <div>${user.created_at}</div>
+                          <div id = 'icons'> 
+                          <i class="fas fa-flag i1 fa-2x"></i>
+                          <i class="fas fa-retweet i2 fa-2x"></i>
+                          <i class="fab fa-gratipay i3 fa-2x"></i>
+                          </div>
+                         </footer>
+                      </article>
+  `
+  return tweetBody;
 }
 
 
